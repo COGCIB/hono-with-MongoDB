66e39e24bf5208c3c5ce2505a999570dbe6138a4
Fekadu Debebe <debebefekadu14@gmail.com>
2024-09-02 14:57:12 +0300

    Update connect.ts

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export default async function dbConnect() {
-  await mongoose.connect("mongodb://localhost:27017");
+  await mongoose.connect(
+    "mongodb+srv://mmehari:Showmeariver2023@cluster0.rfn7yp7.mongodb.net/hiteshDB?retryWrites=true&w=majority&appName=Cluster0"
+  );
   console.log("Monggodb connected successfully...");
 }
